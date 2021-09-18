271a11af50dc11330034bd2ff1508ded0fb8dfb7
erickoficial69 <erickoficial69@gmail.com>
2021-09-18 15:01:40 -0400

    integrado cloudinary

--- src/express/controllers/media_files.controller.ts
+++ src/express/controllers/media_files.controller.ts
@@ -18,28 +18,20 @@ export const create_file:RequestHandler = async(req,res)=>{
 
     for(let i=0;i<files.length;i++ ){
         if(!process.env.NODE_ENV || process.env.NODE_ENV !== 'production'){
-            files[i].url = express.domains.backend_cellunatic+'/'+files[i].path.replace('puclic','')
+            files[i].url = express.domains.backend_cellunatic+files[i].path.replace('public','')
             await Media_files.create(files[i])
         }
         if(process.env.NODE_ENV || process.env.NODE_ENV == 'production'){
             try{
                 let resp = await cloudinary.v2.uploader.upload(files[i].path)
                 files[i].url = resp.secure_url
-                cloudinary.v2.image ( resp.secure_url , { transformación : [
-                    { gravedad : " cara " , altura : 200 , ancho : 200 , recorte : " pulgar " },
-                    { borde : " 5px_solid_black " , radio : 20 },
-                    { overlay : " cloudinary_icon_white " },
-                    { banderas : " relativo " , ancho : " 0.25 " , recorte : " escala " },
-                    { opacidad : 50 },
-                    { flags : " layer_apply " , gravity : " north_east " , x : 10 , y : 10 }
-                    ]})
                 await Media_files.create(files[i])
-                promises.unlink(files[i].path)
+                await promises.unlink(files[i].path)
             }catch(err){
+                await promises.unlink(files[i].path)
                 console.error(err)
             }
         }
-        
     }
     return res.json(files)
 }
--- src/express/routes/index.routes.ts
+++ src/express/routes/index.routes.ts
@@ -1,11 +1,23 @@
 import {Response, Router,Request} from 'express'
 import upload_cover from '../../plugins/multer_img'
 import * as file_ctrl from '../controllers/media_files.controller'
+import {twiml} from 'twilio'
 const router = Router()
 
+const MessagingResponse = twiml.MessagingResponse
 router.get('/',(_:Request,res:Response)=>{
-    res.send(process.env.NODE_ENV)
+    
+    return res.send('https://0786-185-147-55-10.ngrok.io')
 })
+
+router.post('/',(req:Request,res:Response)=>{
+    const twiml = new MessagingResponse();
+    console.log(twiml.toString())
+    console.log(req.body)
+    return res.json(twiml.toString())
+})
+
 router.post('/covers', upload_cover.array('cover'), file_ctrl.create_file)
 router.delete('/covers', file_ctrl.delete_file)
+
 export default router
